4f3481953c5f8ad52517af4ce4c988059e35368a
Maxwell Baird <bairdm71@gmail.com>
2020-05-19 10:33:07 -0600

    made the test cover more options

--- spec/features/user_can_search_spec.rb
+++ spec/features/user_can_search_spec.rb
@@ -11,10 +11,20 @@ describe 'visitor' do
 
     expect(current_path).to eq('/search')
     expect(page).to have_content("21 members from Gryffindor")
-    expect(page).to have_content("Aberforth Dumbledore Gryffindor Owner, Hog's Head Inn goat")
+    expect(page).to have_content("Aberforth Dumbledore")
     expect(page).to have_content("House: Gryffindor")
     expect(page).to have_content("Role: Owner, Hog's Head Inn")
     expect(page).to have_content("Patronus: goat")
+
+    visit '/'
+    find('#house').find(:xpath, 'option[3]').select_option
+
+    click_on 'Search For Members'
+    expect(page).to have_content("1 members from Slytherin")
+    expect(page).to have_content("Severus Snape")
+    expect(page).to have_content("House: Slytherin")
+    expect(page).to have_content("Role: Professor, Potions")
+    expect(page).to have_content("Patronus: doe")
   end
 
 end
